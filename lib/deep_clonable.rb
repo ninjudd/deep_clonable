5363809c8722242f68051622f56723b8dfbabd6c
ninjudd <git@justinbalthrop.com>
2011-06-02 17:13:50 -0700

    add support for frozen objects

--- lib/deep_clonable.rb
+++ lib/deep_clonable.rb
@@ -8,25 +8,37 @@ end
 module DeepClonable
   module InstanceMethods
     def clone
-      cloned_object = super
-      cloned_object.clone_fields
-      cloned_object
+      was_frozen = frozen?
+      cloned_object = super.dup
+      cloned_object = yield(cloned_object) if block_given?
+      was_frozen ? cloned_object.freeze : cloned_object
     end
-    
-    def dup
-      cloned_object = super
-      cloned_object.clone_fields
+
+    def dup(method = :dup)
+      cloned_object = super()
+      cloned_object.update_vars(deep_vars, method)
       cloned_object
     end
 
-    # You can override clone_fields in your class to do deep clone in place.
+    alias old_freeze freeze
+
+    def freeze
+      dup(:freeze).old_freeze
+    end
+
+    # You can override deep_vars in your class to specify which instance_variables should be deep cloned.
     # As it is, all Arrays and Hashes are deep cloned.
-    def clone_fields
-      instance_variables.each do |variable|
-        value = instance_variable_get(variable)
-        if value.kind_of?(Array) or value.kind_of?(Hash) or value.kind_of?(DeepClonable::InstanceMethods)
-          instance_variable_set(variable, value.clone)
-        end
+    def deep_vars
+      instance_variables.select do |var|
+        value = instance_variable_get(var)
+        value.kind_of?(Array) or value.kind_of?(Hash) or value.kind_of?(DeepClonable::InstanceMethods)
+      end
+    end
+
+    def update_vars(vars, method)
+      vars.each do |var|
+        value = instance_variable_get(var)
+        instance_variable_set(var, value.send(method))
       end
     end
   end
@@ -34,8 +46,8 @@ module DeepClonable
   module ClassMethods
     # Use this to define cloning version of the given in-place methods.
     #
-    # This method supports a common idiom where you create in-place and 
-    # cloning versions of most operations. This method allows you to 
+    # This method supports a common idiom where you create in-place and
+    # cloning versions of most operations. This method allows you to
     # define only the in-place version (with the trailing bang (!),
     # that part is important) and specify the cloning versions by name
     # using this method.
@@ -43,13 +55,10 @@ module DeepClonable
       clone_method_name = clone_method_name.to_s.gsub(/\!$/,'')
       method_name ||= "#{clone_method_name}!"
       class_eval %{
-        def #{clone_method_name}(*args)
-          object = self.clone
-          object.#{method_name}(*args)
-          if block_given?
-            yield object
-          else
-            object
+        def #{clone_method_name}(*args, &block)
+          self.clone do |object|
+            object.#{method_name}(*args)
+            block ? block.call(object) : object
           end
         end
       }
--- test/deep_clonable_test.rb
+++ test/deep_clonable_test.rb
@@ -1,41 +1,100 @@
-require File.dirname(__FILE__) + '/test_helper'
+require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestClass
   deep_clonable
-  
-  attr_reader :num
-  
-  def initialize(n)
-    @num = n
+
+  attr_reader :num, :hash, :array
+
+  def initialize(num, hash, array)
+    @num   = num
+    @hash  = hash
+    @array = array
   end
-  
+
   clone_method :+, :add!
   def add!(other)
     @num += other.num
+    @hash.merge!(other.hash)
+    @array.concat(other.array)
   end
 
   clone_method :invert
   def invert!
-    @num = -(@num)
+    @num  = -(@num)
+    @hash = @hash.invert
+    @array.reverse!
   end
 end
 
 class DeepClonableTest < Test::Unit::TestCase
-  should 'clone method' do
-    a = TestClass.new(10)
+  should "clone method" do
+    a = TestClass.new(10, {:a => 1}, [1,2,3])
     b = a.invert
-    
-    assert_equal 10,  a.num
-    assert_equal 10, -b.num
+
+    assert_equal(10,        a.num)
+    assert_equal({:a => 1}, a.hash)
+    assert_equal([1,2,3],   a.array)
+
+    assert_equal(-10,       b.num)
+    assert_equal({1 => :a}, b.hash)
+    assert_equal([3,2,1],   b.array)
   end
 
   should 'clone method with rename' do
-    a = TestClass.new(10)
-    b = TestClass.new(20)
+    a = TestClass.new(10, {:a => 1}, [1])
+    b = TestClass.new(20, {:b => 2}, [2,3])
     c = a + b
-    
-    assert_equal 30, c.num
-    assert_equal 20, b.num
-    assert_equal 10, a.num
+
+    assert_equal(10,        a.num)
+    assert_equal({:a => 1}, a.hash)
+    assert_equal([1],       a.array)
+
+    assert_equal(20,        b.num)
+    assert_equal({:b => 2}, b.hash)
+    assert_equal([2,3],     b.array)
+
+    assert_equal(30,                 c.num)
+    assert_equal({:a => 1, :b => 2}, c.hash)
+    assert_equal([1,2,3],            c.array)
+  end
+
+  should "return a frozen object when cloning a frozen object" do
+    a = TestClass.new(10, {:a => 1}, [1,2,3]).freeze
+
+    assert_raises(TypeError) do
+      a.invert!
+    end
+
+    b = a.invert
+
+    assert_equal(10,        a.num)
+    assert_equal({:a => 1}, a.hash)
+    assert_equal([1,2,3],   a.array)
+
+    assert_equal(-10,       b.num)
+    assert_equal({1 => :a}, b.hash)
+    assert_equal([3,2,1],   b.array)
+
+    assert a.frozen?
+    assert b.frozen?
+    assert a.clone.frozen?
+  end
+
+  should "unfreeze an object when duping" do
+    a = TestClass.new(10, {:a => 1}, [1,2,3]).freeze
+    b = a.dup
+
+    assert  a.frozen?
+    assert !b.frozen?
+
+    b.invert!
+
+    assert_equal(10,        a.num)
+    assert_equal({:a => 1}, a.hash)
+    assert_equal([1,2,3],   a.array)
+
+    assert_equal(-10,       b.num)
+    assert_equal({1 => :a}, b.hash)
+    assert_equal([3,2,1],   b.array)
   end
 end
